a5be4a673db0abf4a28290ead09e609cbd37b399
Mark Watts <MarkWatts@boomi.com>
2019-12-17 15:12:06 +0000

    Updated to FlowComponentModel spec

--- src/OutcomeManager.tsx
+++ src/OutcomeManager.tsx
@@ -1,96 +1,94 @@
-declare var manywho: any;
+import '/Operational Data/Flow UI Custom Components/2019 Version/FlowComponentModel/src/EventManager';
+declare const manywho: any;
 
-import * as React from 'react';
+/*
+let originalRequest: any;
+let selectedOutcome: string = '';
+let stateId: string;
+let stateToken: string;
+let currentMapElement: string;
+let tenantId: string;
+*/
+
+let handlers: {[key: string] : any} = [];
+handlers["AutoOpen"] = OMautoOpen;
+handlers["AutoNav"] = OMautoNav;
+handlers["AutoClose"] = OMautoClose;
+handlers["AutoPrint"] = OMAutoPrint;
 
 
-var queryParameters = manywho.utils.parseQueryString(window.location.search.substring(1));
-var tenantId = queryParameters['tenant-id'];
-if (!tenantId) 
+manywho.eventManager.addBeforeSendListener(OMbeforeSendHandler,"outcome-manager");
+//manywho.eventManager.addDoneListener(OMdoneHandler, "outcome-manager");
+
+//this saves the last selected outcome in the OutcomeManager           
+function OMbeforeSendHandler(xhr: any, request: any )
 {
-    tenantId = window.location.pathname
-                .split('/')
-                .filter(function (path) 
-                {
-                    return path && path.length > 0;
-                })[0];
+    if(request)
+    {
+        let message: any = {};
+        message.stateId = request.stateId;
+        message.stateToken = request.stateToken;
+        message.currentMapElement = request.currentMapElementId;
+        message.originalRequest  = request;
+        message.tenantId = window.location.pathname.split("\/")[1];
+        message.outcomeId = request.mapElementInvokeRequest.selectedOutcomeId;
+        message.baseUrl = window.location.origin;
+
+        if(message.tenantId && message.stateId && message.outcomeId )
+        {
+            message.flowKey = message.tenantId + "___" + message.stateId;
+            message.outcome = manywho.model.getOutcome( message.outcomeId  ,  message.flowKey );
+
+            if(message.outcome) {
+                let attributes: {[name: string]: string} = message.outcome.attributes;
+                if(attributes) {
+                    Object.keys(attributes).forEach((attributeName: string) => {
+                        if(handlers[attributeName] && attributes[attributeName].trim().toLowerCase() === "true") {
+                            handlers[attributeName](message, xhr, request); 
+                        }
+                    });
+                }
+            }
+        }
+    }
 }
-    
 
-
-
-//declare global { interface Window {OutcomeManager: any;}}
-manywho.OutcomeManager = {};
-manywho.OutcomeManager.tenantId  = tenantId;
-manywho.OutcomeManager.originalRequest;
-manywho.OutcomeManager.handlers = {};
-manywho.OutcomeManager.handlers["AutoClose"] = OMautoClose;
-manywho.OutcomeManager.handlers["AutoOpen"] = OMautoOpen;
-manywho.OutcomeManager.handlers["AutoNav"] = OMautoNav;
-manywho.OutcomeManager.handlers["ForcePrint"] = OMforcePrint;
-
-
-for(var key in manywho.OutcomeHandlers)
+/*
+function OMdoneHandler(xhr: any , request: any)
 {
-    manywho.OutcomeManager.handlers[key] = manywho.OutcomeHandlers[key];
+    //check close flag and if set just kill the window
+   // if( manywho.OutcomeManager && manywho.OutcomeManager.selectedOutcome && manywho.OutcomeManager.selectedOutcome.attributes )
+    //{
+        //loop over each outcome and call its associated handler in the player if it exists
+        //some default common ones will default to the handlers in  here if not.
+        //for( var key in manywho.OutcomeManager.selectedOutcome.attributes )
+        //{
+            //if(manywho.OutcomeManager.handlers[key] && manywho.OutcomeManager.selectedOutcome.attributes[key] === "true")
+            //{
+            //    var functionName = manywho.OutcomeManager.handlers[key];
+            //    manywho.OutcomeManager.handlers[key](manywho.OutcomeManager.selectedOutcome, xhr, manywho.OutcomeManager.originalRequest);
+           // }
+            
+        //}
+    //}
 }
-    
-// this attaches the Ajax handlers to the manywho.CustomComponentOrchestrator or 
-// initialize function if manywho.CustomComponentOrchestrator isnt defined. 
-if (!(manywho as any).eventManager) {
-    (manywho as any).eventManager = {};
-    (manywho as any).eventManager.beforeSendListeners = [];
-    (manywho as any).eventManager.doneListeners = [];
-    (manywho as any).eventManager.failListeners = [];
+*/
 
-    (manywho as any).eventManager.beforeSend = (xhr: XMLHttpRequest, request: any) => {
-        (manywho as any).eventManager.beforeSendListeners.forEach((listener: any) => listener(xhr, request));
-    };
 
-    (manywho as any).eventManager.done = (xhr: XMLHttpRequest, request: any) => {
-        (manywho as any).eventManager.doneListeners.forEach((listener: any) => listener(xhr, request));
-    };
-
-    (manywho as any).eventManager.fail = (xhr: XMLHttpRequest, request: any) => {
-        (manywho as any).eventManager.failListeners.forEach((listener: any) => listener(xhr, request));
-    };
-
-    (manywho as any).eventManager.addBeforeSendListener = (handler: (xhr: XMLHttpRequest, request: any) => void) => {
-        (manywho as any).eventManager.beforeSendListeners.push(handler);
-    };
-
-    (manywho as any).eventManager.addDoneListener = (handler: (xhr: XMLHttpRequest, request: any) => void) => {
-        (manywho as any).eventManager.doneListeners.push(handler);
-    };
-
-    (manywho as any).eventManager.addFailListener = (handler: (xhr: XMLHttpRequest, request: any) => void) => {
-        (manywho as any).eventManager.failListeners.push(handler);
-    };
-
-    manywho.settings.initialize(null, {
-        invoke: {
-            beforeSend: (manywho as any).eventManager.beforeSend,
-            done: (manywho as any).eventManager.done,
-            fail: (manywho as any).eventManager.fail,
-        },
-    });
-}
-
-manywho.eventManager.addBeforeSendListener(OMbeforeSendHandler);
-manywho.eventManager.addDoneListener(OMdoneHandler);
-
-function OMautoClose(outcome : any, xhr : any, request: any)
+function OMautoClose(message : any, xhr : any, request: any)
 {
     window.close();
     return;
 }
 
-function OMautoNav(outcome : any, xhr : any, request: any)
+async function OMautoNav(message : any, xhr : any, request: any)
 {
-    var targetUrl = outcome.attributes.AutoNavUrl || "";
+    var targetUrl = message.outcome.attributes.AutoNavUrl || "";
 
     if(targetUrl && targetUrl.length > 0)
     {
-        var wnd = window.open(targetUrl, "_self");
+        var url = await getValue(message, targetUrl)
+        var wnd = window.open(url, "_self");
     }
     else
     {
@@ -100,100 +98,51 @@ function OMautoNav(outcome : any, xhr : any, request: any)
 
 
 
-function OMautoOpen(outcome : any, xhr : any, request: any)
+async function OMautoOpen(message: any, xhr : any, request: any)
 {
 
-    var targetUrl = outcome.attributes.AutoOpenUrl || "";
+    var targetUrl = message.outcome.attributes.AutoOpenUrl || "";
 
     if(targetUrl && targetUrl.length > 0)
     {
-        var wnd = window.open(targetUrl, "_blank");
+        var url = await getValue(message, targetUrl)
+        var wnd = window.open(url, "_blank");
     }
     else
     {
         alert("No AutoOpenUrl specified in the outcome's attributes");
     }
 
-    /*
-    //the outcome's .pageObjectBindingId tells us which component triggered the outcome and inserted the data
-    var triggerComponent=outcome.pageObjectBindingId;
-
-    //get the response for the correct component
-    var response : any;
-    for(var pos = 0 ; pos < request.mapElementInvokeRequest.pageRequest.pageComponentInputResponses.length ; pos++)
-    {
-        var item = request.mapElementInvokeRequest.pageRequest.pageComponentInputResponses[pos];
-        if(item.pageComponentId === triggerComponent)
-        {
-            response = item;
-            break;
-        }
-    }
-
-    if(response)
-    {
-        var targetUri = response.contentValue;
-        var wnd = window.open(targetUri, "_blank");
-    }
-    else
-    {
-        alert("failed to get uri - pageObjectBindingId not found in pageComponentInputResponses");
-    }
-    */
 }
 
-function OMforcePrint(outcome : any, xhr : any, request: any)
+function OMAutoPrint(message : any, xhr : any, request: any)
 {
     window.print();
     return;
 }
     
-function OMdoneHandler(xhr: any , request: any)
-{
-    //check close flag and if set just kill the window
-    if( manywho.OutcomeManager && manywho.OutcomeManager.selectedOutcome && manywho.OutcomeManager.selectedOutcome.attributes )
-    {
-        //loop over each outcome and call its associated handler in the player if it exists
-        //some default common ones will default to the handlers in  here if not.
-        for( var key in manywho.OutcomeManager.selectedOutcome.attributes )
-        {
-            if(manywho.OutcomeManager.handlers[key] && manywho.OutcomeManager.selectedOutcome.attributes[key] === "true")
-            {
-                var functionName = manywho.OutcomeManager.handlers[key];
-                manywho.OutcomeManager.handlers[key](manywho.OutcomeManager.selectedOutcome, xhr, manywho.OutcomeManager.originalRequest);
-            }
-            
+
+
+async function getValue(message: any, value: string): Promise<string> {
+
+    if(value.trim().startsWith("{{") && value.trim().endsWith("}}")) {
+        let stripped: string = value.replace('{{', '');
+        stripped = stripped.replace('}}', '');    
+        const valueurl = `${message.baseUrl}/api/run/1/state/${message.stateId}/values/name`;
+        const val: any = await manywho.connection.request(null, "", valueurl + "/" + stripped , 'GET', message.tenantId, message.stateId, manywho.state.getAuthenticationToken(message.flowKey), {});
+        if(val.developerName) { 
+            console.log("Value '" + stripped + "' contained '" + val.contentValue + "'");
+            return val.contentValue as string;
         }
-    }
-}
-
-//this saves the last selected outcome in the OutcomeManager           
-function OMbeforeSendHandler(xhr: any, request: any )
-{
-    if(request)
-    {
-        manywho.OutcomeManager.originalRequest  = request;
-        manywho.OutcomeManager.stateId = request.stateId;
-        var outcomeId = request.mapElementInvokeRequest.selectedOutcomeId;
-        if(manywho.OutcomeManager.tenantId && manywho.OutcomeManager.stateId && outcomeId )
-        {
-            var flowKey = manywho.OutcomeManager.tenantId + "___" + manywho.OutcomeManager.stateId;
-            manywho.OutcomeManager.selectedOutcome = manywho.model.getOutcome( outcomeId  ,  flowKey );
+        else {
+            console.log("No value named '" + stripped + "' esists in the flow");
+            return "";
         }
+        
     }
-    else
-    {
-        manywho.OutcomeManager.originalRequest  = null;
-        manywho.OutcomeManager.stateId = "";
-        manywho.OutcomeManager.selectedOutcome = null;
+    else {
+        return value.trim();
     }
 }
 
-class OutcomeManager extends React.Component<any, any> 
-{
-}
-
-manywho.component.register('OutcomeManager', OutcomeManager);
-
-export default OutcomeManager;
 
